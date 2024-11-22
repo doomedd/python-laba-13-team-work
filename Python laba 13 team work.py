d94019e34b9e6c86fbb5fe09508c98bd9269c11d
Руденко Данііл <danichrudenko@gmail.com>
2024-11-22 21:41:15 +0200

    Update Python laba 13 team work.py

--- Python laba 13 team work.py
+++ Python laba 13 team work.py
@@ -20,3 +20,28 @@ def create_csv_file():
     except Exception as e:
         print(f"Помилка при створенні CSV-файлу: {e}")
 
+# (Руденко Данііл КН-33.2)
+# Функція для конвертації CSV у JSON
+def csv_to_json():
+    try:
+        data = {}
+
+        # Читання CSV файлу
+        with open('books.csv', mode='r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                row['year'] = int(row['year'])  # Рік публікації (int)
+                # Використовуємо назву книги як ключ для словника
+                data[row['title']] = {"author": row['author'], "year": row['year']}
+
+        # Запис у JSON файл
+        with open('books.json', mode='w') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+        print("JSON файл 'books.json' успішно створено.")
+
+    except FileNotFoundError:
+        print("Помилка: Файл 'books.csv' не знайдено.")
+    except json.JSONDecodeError:
+        print("Помилка: Не вдалося декодувати JSON.")
+    except Exception as e:
+        print(f"Невідома помилка: {e}")
